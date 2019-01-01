ecae78251ff2e750653079fe5514206ee16fd03f
qq <w@p>
2019-01-01 14:37:47 +0100

    kodanie kurki

---
+++ src/components/Kurka.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="kurka">
+    {{msg}}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Kurka',
+  data () {
+    return {
+      msg: 'Kurde'
+    }
+  }
+}
+</script>
